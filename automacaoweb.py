a4429a8493040dadc2d89304e612e46d543fdeaa
Heerdyy <anajulia22m@gmail.com>
2025-08-14 13:10:57 -0300

    login

--- automacaoweb.py
+++ automacaoweb.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
-upd_navegador = Service(ChromeDriverManager().install()) # várivael que será instânciada e fará uma checkagem da versão do webdriver do chrome para a utilização e retornará o path do arquivo para o driver caso esteja em uma versão desatualizada
+upd_navegador = Service(ChromeDriverManager().install()) # váriavel que será instânciada e fará uma checkagem da versão do webdriver do chrome para a utilização e retornará o path do arquivo para o driver caso esteja em uma versão desatualizada
 navegador = webdriver.Chrome(service=upd_navegador) # objeto que será utilizado para a interagir com o navegador 
 
 navegador.get('https://practicetestautomation.com/practice-test-login/') # abre o site
